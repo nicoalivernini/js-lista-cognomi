1b854b791cdb71c8d7990a9d9c7686b6cb231d07
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-04 15:46:49 +0100

    Ordinati alfabeticamente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
-console.log("ok");
+// Chiedere all’utente il cognome
+// inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Perlari’, ‘Lodigiani’, 'Mastrobattista', ‘Verdi’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+
+//Chiedo cognome
+var cognomeUtente = prompt("Inserisci il tuo cognome");
+
+//Creo array con altri cognomi
+var listaCognomi = ["Bianchi", "Rossi", "Perlari", "Lodigiani", "Mastrobattista", "Verdi", cognomeUtente];
+
+//Metto in ordine alfabetico
+listaCognomi.sort();
+
+//Stampo
+console.log(listaCognomi);
+
+//Scrivo la posizione umana della lista
+console.log(listaCognome.value(););
